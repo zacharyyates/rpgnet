0190d21186775cc1861ceea8baf4b02ad06aff9f
zachary.yates <zachary.yates@6d4b1071-324f-0410-8fd9-59f6c26b83b8>
2008-07-18 20:40:47 +0000

    Added WikiController

---
+++ trunk/Rpg.Web/Controllers/WikiController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Rpg.Web.Controllers
+{
+	public class WikiController : Controller
+	{
+		public ActionResult Index()
+		{
+			// Add action logic here
+			throw new NotImplementedException();
+		}
+	}
+}
