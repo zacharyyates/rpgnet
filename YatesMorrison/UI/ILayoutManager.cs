5a42850dce08f018f931b844f724c8eeeedb4269
zachary.yates <zachary.yates@6d4b1071-324f-0410-8fd9-59f6c26b83b8>
2009-02-25 16:51:10 +0000

    Added ILayoutManager.cs Made MvcSubmitButton inherit from IButtonControl

---
+++ YatesMorrison/UI/ILayoutManager.cs
@@ -0,0 +1,16 @@
+﻿/* Zachary Yates
+ * Copyright © 2009 YatesMorrison Software Company
+ * 2.22.2009
+ */
+namespace YatesMorrison.UI
+{
+	using System.Collections.Generic;
+
+	/// <summary>
+	/// Defines generic layout schemes for scaffolding
+	/// </summary>
+	public interface ILayoutManager
+	{
+			
+	}
+}
