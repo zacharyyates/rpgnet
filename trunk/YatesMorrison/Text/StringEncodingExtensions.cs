457bc100dcf65f7839ecea0f2074fe2f90cf48f0
zachary.yates <zachary.yates@6d4b1071-324f-0410-8fd9-59f6c26b83b8>
2009-05-11 20:29:57 +0000

    Added files that were missing from version control.

---
+++ trunk/YatesMorrison/Text/StringEncodingExtensions.cs
@@ -0,0 +1,51 @@
+﻿/* Zachary Yates
+ * Copyright © 2009 YatesMorrison Software Company
+ * 2.26.2009
+ */
+namespace YatesMorrison.Text
+{
+	using System.Text;
+
+	public static class StringEncodingExtensions
+	{
+		public static byte[] ToByte(this string str)
+		{
+			return Encoding.Default.GetBytes(str);
+		}
+		public static byte[] ToByte(this string str, Encoding encoding)
+		{
+			return encoding.GetBytes(str);
+		}
+		public static byte[] ToByte(this string str, EncodingType encoding)
+		{
+			return GetEncoding(encoding).GetBytes(str);
+		}
+
+		static Encoding GetEncoding(EncodingType textEncoding)
+		{
+			switch (textEncoding)
+			{
+				case EncodingType.ASCII: return Encoding.ASCII;
+				case EncodingType.BigEndianUnicode: return Encoding.BigEndianUnicode;
+				case EncodingType.Unicode: return Encoding.Unicode;
+				case EncodingType.UTF32: return Encoding.UTF32;
+				case EncodingType.UTF7: return Encoding.UTF7;
+				case EncodingType.UTF8: return Encoding.UTF8;
+				
+				default:
+				case EncodingType.Default: return Encoding.Default;
+			}
+		}
+	}
+
+	public enum EncodingType
+	{
+		ASCII,
+		BigEndianUnicode,
+		Default,
+		Unicode,
+		UTF32,
+		UTF7,
+		UTF8,
+	}
+}
