e8a9dc763192fe6e6a9eccc719b7d2902596d917
zachary.yates <zachary.yates@6d4b1071-324f-0410-8fd9-59f6c26b83b8>
2009-05-11 20:30:26 +0000

    Added files that were missing from version control.

---
+++ YatesMorrison.Tests/StringHashingExtensionsTest.cs
@@ -0,0 +1,43 @@
+﻿namespace YatesMorrison.Tests
+{
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+	using YatesMorrison.Security.Cryptography;
+	using YatesMorrison.Text;
+
+	[TestClass]
+	public class StringHashingExtensionsTest
+	{
+		public TestContext TestContext { get; set; }
+
+		[TestMethod]
+		public void TestToHash1()
+		{
+			string plain = "This is a plain string !@#$%";
+			string hashed = plain.ToHash();
+			Assert.AreNotEqual(plain, hashed);
+			TestContext.WriteLine("Originial {0}, Hashed {1}", plain, hashed);
+			Assert.IsTrue(plain.VerifyAgainstHash(hashed));
+		}
+
+		[TestMethod]
+		public void TestToHashSHA512()
+		{
+			string plain = "This is a plain string !@#$%";
+			string hashed = plain.ToHash(HashAlgorithmType.SHA512);
+			Assert.AreNotEqual(plain, hashed);
+			TestContext.WriteLine("Originial {0}, Hashed {1}", plain, hashed);
+			Assert.IsTrue(plain.VerifyAgainstHash(hashed, HashAlgorithmType.SHA512));
+		}
+
+		[TestMethod]
+		public void TestToHashWithSalt()
+		{
+			string plain = "This is a plain string !@#$%";
+			byte[] saltBytes = "SALT".ToByte(EncodingType.UTF8);
+			string hashed = plain.ToHash(HashAlgorithmType.SHA512, saltBytes);
+			Assert.AreNotEqual(plain, hashed);
+			TestContext.WriteLine("Originial {0}, Hashed {1}", plain, hashed);
+			Assert.IsTrue(plain.VerifyAgainstHash(hashed, HashAlgorithmType.SHA512, saltBytes));
+		}
+	}
+}
